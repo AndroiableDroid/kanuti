eadc90d5c49c1af2cbdbffd97a00b083d91067b8
Angelo G. Del Regno <kholk11@gmail.com>
2016-06-28 17:37:39 +0200

    arm64: asm: tlbflush: Change all instances of dsb() to dsb(sy)
    The semantics of dsb on ARM is different from AArch64.

--- arch/arm64/include/asm/tlbflush.h
+++ arch/arm64/include/asm/tlbflush.h
@@ -106,9 +106,9 @@ static inline void flush_tlb_all(void)
 static inline void flush_tlb_mm(struct mm_struct *mm)
 {
 #ifdef CONFIG_ARCH_MSM8994_V1_TLBI_WA
-	dsb();
+	dsb(sy);
 	__tlbi(vmalle1is);
-	dsb();
+	dsb(sy);
 	isb();
 #else
 	unsigned long asid = (unsigned long)ASID(mm) << 48;
@@ -124,9 +124,9 @@ static inline void flush_tlb_page(struct vm_area_struct *vma,
 				  unsigned long uaddr)
 {
 #ifdef CONFIG_ARCH_MSM8994_V1_TLBI_WA
-	dsb();
+	dsb(sy);
 	__tlbi(vmalle1is);
-	dsb();
+	dsb(sy);
 	isb();
 #else
 	unsigned long addr = uaddr >> 12 |
